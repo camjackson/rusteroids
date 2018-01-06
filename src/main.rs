e94a14e27a24672df26a9a953005c22d367789bc
Cam Jackson <camjackson89@gmail.com>
2018-01-06 15:38:28 +0800

    Split up the main function a bit

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,10 @@ extern crate compact;
 extern crate compact_macros;
 
 use std::net::{SocketAddr, IpAddr, Ipv4Addr};
-use kay::{Networking, ActorSystem, Actor};
-use monet::RenderableID;
+use kay::{Networking, World, ActorSystem, Actor};
+use monet::{RendererID, RenderableID};
 use monet::glium::glutin::WindowBuilder;
+use stagemaster::UserInterfaceID;
 
 mod game_object;
 mod thing;
@@ -18,39 +19,18 @@ use game_object::{GameID, GameObjectID};
 use thing::{Thing, ThingID};
 
 fn main() {
-    let mut actor_system = actor_system();
+    // Initialise actor system
+    let actor_system = &mut actor_system();
     let world = &mut actor_system.world();
     actor_system.networking_connect();
 
     // Initialise actor types
-    thing::setup(&mut actor_system);
-    game_object::setup(&mut actor_system);
+    thing::setup(actor_system);
+    game_object::setup(actor_system);
 
-    // Initialise game
-    let game_object_broadcast_ids: Vec<GameObjectID> = vec![
-        Thing::local_broadcast(world).into(),
-    ].into();
-    let game = GameID::spawn(game_object_broadcast_ids.into(), world);
-
-    // Initialise renderer
-    let renderable_broadcast_ids: Vec<RenderableID> = vec![];
-    let environment = stagemaster::environment::Environment {
-        name: "Rusteroids",
-        version: "0.0.1",
-        author: "Cam Jackson",
-    };
-    let window_builder = WindowBuilder::new()
-        .with_title(environment.name)
-        .with_dimensions(1024, 768)
-        .with_multitouch();
-    let clear_color = (1., 0., 1., 1.0);
-    let (ui, renderer) = stagemaster::setup(
-        &mut actor_system,
-        renderable_broadcast_ids.into(),
-        environment,
-        window_builder,
-        clear_color,
-    );
+    // Initialise game and graphics
+    let game = game(world);
+    let (ui, renderer) = graphics(actor_system);
 
     // Hello, actor system!
     let thing = ThingID::spawn(world);
@@ -74,4 +54,34 @@ fn actor_system() -> ActorSystem {
     let localhost_8080 = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 8080);
     let networking = Networking::new(machine_id, vec!(localhost_8080));
     ActorSystem::new(networking)
+}
+
+fn game(world: &mut World) -> GameID {
+    let game_object_broadcast_ids: Vec<GameObjectID> = vec![
+        Thing::local_broadcast(world).into(),
+    ].into();
+    GameID::spawn(game_object_broadcast_ids.into(), world)
+}
+
+fn graphics(actor_system: &mut ActorSystem,) -> (UserInterfaceID, RendererID) {
+    let renderable_broadcast_ids: Vec<RenderableID> = vec![
+
+    ];
+    let env = stagemaster::environment::Environment {
+        name: "Rusteroids",
+        version: "0.0.1",
+        author: "Cam Jackson",
+    };
+    let window_builder = WindowBuilder::new()
+        .with_title(env.name)
+        .with_dimensions(1024, 768)
+        .with_multitouch();
+    let clear_color = (1., 0., 1., 1.0);
+    stagemaster::setup(
+        actor_system,
+        renderable_broadcast_ids.into(),
+        env,
+        window_builder,
+        clear_color,
+    )
 }
